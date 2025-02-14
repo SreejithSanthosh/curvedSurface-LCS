2843d28bf48b9fa4eef903db7a2ab16d32cddc74
Sreejith Santhosh <48742875+SreejithSanthosh@users.noreply.github.com>
2025-02-13 23:39:41 -0800

    After checking active nemaqtic flow stationary

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ addpath(fullfile(cd,'LagrangianDeform'));
 addpath(fullfile(cd,'miscFtns'));
 
 % % Load the data and generate a KDTree 
-load(fullfile(cd,'Data','staticMesh.mat'));  Nt = size(time,2);
+load(fullfile(cd,'Data','staticMesh.mat'));  Nt = size(timeArr,2);
 %% Parameters- NEEDS USER INPUT  
 
 isStatic = 1; % 1- isStatic Mesh on the data, 0 - Meshdeforms/changes in every time
@@ -17,7 +17,7 @@ cpu_num = 2; % The number of parallel cpu cores to be alloted for advection
 Nplot = 20;  % How many frames are being plotted on the videos generated in this code 
 fntSz = 30; % FontSize of the plots 
 
-% We compute the FTLE for trajectories from time(ct_i) -> time(ct_f)
+% We compute the FTLE for trajectories from timeArr(ct_i) -> timeArr(ct_f)
 % time array is loaded in along with the data 
 ct_f = Nt; ct_i = 1; dt = 1*10^(-1);
 
@@ -56,7 +56,7 @@ figure('Color','w','units','normalized','outerposition',[0 0 1 1]);
 
 for idXMesh=1:round(Nt/Nplot):Nt
     count = count+1;
-    t_i = time(idXMesh);
+    t_i = timeArr(idXMesh);
 
     v1Data = squeeze(v{1,idXMesh}); v2Data = squeeze(v{2,idXMesh}); v3Data = squeeze(v{3,idXMesh});
     xData = squeeze(x{idXMesh}); yData = squeeze(y{idXMesh}); zData = squeeze(z{idXMesh});
@@ -85,34 +85,34 @@ writeVideo(writerObj,mov); close(writerObj);
 %% Advect the grid backward 
 clc; close;
 
-tau0_Advect = 0; tauf_Advect = time(ct_f)-time(ct_i); 
+tau0_Advect = 0; tauf_Advect = timeArr(ct_f)-timeArr(ct_i); 
 xqBck = x{ct_f}; yqBck = y{ct_f}; zqBck = z{ct_f}; % Initial condition for backward advect 
 xqFor = x{ct_i}; yqFor = y{ct_i}; zqFor = z{ct_i}; % Initial condition for forward advect
 
-tf_Absolute = time(ct_f);
+tf_Absolute = timeArr(ct_f);
 tauArr_Advect = tau0_Advect:dt:tauf_Advect;
 tauSave_Advect = tauArr_Advect(1:1:end); 
 
 if isStatic == 1
     [xt_Advect,yt_Advect,zt_Advect] = AdvctBckStatic(cpu_num,tf_Absolute,tauSave_Advect, ...
-        tauArr_Advect,xqBck,yqBck,zqBck,time,v,inputMesh);
+        tauArr_Advect,xqBck,yqBck,zqBck,timeArr,v,inputMesh);
 else
     [xt_Advect,yt_Advect,zt_Advect] = AdvctBck(cpu_num,tf_Absolute,tauSave_Advect, ...
-        tauArr_Advect,xqBck,yqBck,zqBck,time,v,x,y,z,TrianT);
+        tauArr_Advect,xqBck,yqBck,zqBck,timeArr,v,x,y,z,TrianT);
 end 
 
 % % Advect the grid forward
-t0_Advect_for = time(ct_i); tf_Advect_for = time(ct_f);
+t0_Advect_for = timeArr(ct_i); tf_Advect_for = timeArr(ct_f);
 tArr_Advect_for = t0_Advect_for:dt:tf_Advect_for;
 tSave_Advect_for = tArr_Advect_for(1:1:end); 
 
 %Can't save all the data, will save at selective times
 if isStatic == 1
     [xt_Advect_for,yt_Advect_for,zt_Advect_for] = AdvctForStatic(cpu_num,tSave_Advect_for, ...
-        tArr_Advect_for,xqFor,yqFor,zqFor,time,v,inputMesh);
+        tArr_Advect_for,xqFor,yqFor,zqFor,timeArr,v,inputMesh);
 else
     [xt_Advect_for,yt_Advect_for,zt_Advect_for] = AdvctFor(cpu_num,tSave_Advect_for, ...
-        tArr_Advect_for,xqFor,yqFor,zqFor,time,v,x,y,z,TrianT);
+        tArr_Advect_for,xqFor,yqFor,zqFor,timeArr,v,x,y,z,TrianT);
 end 
 
 %% Plot the advection
@@ -130,7 +130,7 @@ for i=1:round(numel(tauSave_Advect)/Nplot):numel(tauSave_Advect)
     t_i = tf_Absolute-tauSave_Advect(i);
 
     % Load the mesh quantities 
-    [~,idXMesh] = min(abs(time-t_i));
+    [~,idXMesh] = min(abs(timeArr-t_i));
     v1Data = -squeeze(v{1,idXMesh}); v2Data = -squeeze(v{2,idXMesh}); v3Data = -squeeze(v{3,idXMesh});
     xData = squeeze(x{idXMesh}); yData = squeeze(y{idXMesh}); zData = squeeze(z{idXMesh});
     TriData = TrianT{idXMesh}; vMag_Data = sqrt(v1Data.^2+v2Data.^2+v3Data.^2);
@@ -165,7 +165,7 @@ for i=1:round(numel(tSave_Advect_for)/Nplot):numel(tSave_Advect_for)
     t_i = tSave_Advect_for(i);
 
     % Load the mesh quantities 
-    [~,idXMesh] = min(abs(time-t_i));
+    [~,idXMesh] = min(abs(timeArr-t_i));
     v1Data = squeeze(v{1,idXMesh}); v2Data = squeeze(v{2,idXMesh}); v3Data = squeeze(v{3,idXMesh});
     xData = squeeze(x{idXMesh}); yData = squeeze(y{idXMesh}); zData = squeeze(z{idXMesh});
     TriData = TrianT{idXMesh}; vMag_Data = sqrt(v1Data.^2+v2Data.^2+v3Data.^2);
@@ -195,7 +195,7 @@ clc; close all; viewAngle = [-107,20];
 
 x0 = squeeze(xt_Advect(1,:)); y0 = squeeze(yt_Advect(1,:)); z0 = squeeze(zt_Advect(1,:));
 xf = squeeze(xt_Advect(end,:)); yf = squeeze(yt_Advect(end,:)); zf = squeeze(zt_Advect(end,:));
-tf = time(ct_i); ti = time(ct_f); % Lagrangian time interval of analysis 
+tf = timeArr(ct_i); ti = timeArr(ct_f); % Lagrangian time interval of analysis 
 
 Tri_pts_Uni = TrianT{ct_f}; % The triangulation of the mesh at tf
 xfData = squeeze(x{1}); yfData = squeeze(y{1}); zfData = squeeze(z{1}); Trif_Data = squeeze(TrianT{1});
@@ -206,20 +206,20 @@ f= figure('color','w','Units','normalized','OuterPosition',[0.0060 0.2435 0.9878
 subplot(1,4,1)
 trisurf(Tri_pts_Uni,x0,y0,z0,lambdaIsoField,'FaceAlpha',1,'Edgecolor','none');
 hold on;  colorbar('southoutside'); axis equal; shading interp;  axis off;
-title(sprintf('$ {}_{iso}\\Lambda_{%.2f}^{%.2f} $',time(ct_f),time(ct_i)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
+title(sprintf('$ {}_{iso}\\Lambda_{%.2f}^{%.2f} $',timeArr(ct_f),timeArr(ct_i)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
 view(viewAngle); camva(7);
 
 subplot(1,4,2)
 trisurf(Tri_pts_Uni,x0,y0,z0,lambdaField,'FaceAlpha',1,'Edgecolor','none'); hold on 
 quiver3(x0,y0,z0,maxDefEigVec(1,:),maxDefEigVec(2,:),maxDefEigVec(3,:),'k','Linewidth',1,'ShowArrowHead','off'); hold off
 axis equal; shading interp; axis off; view(viewAngle); camva(7);
-title(sprintf('$ \\Lambda_{%.2f}^{%.2f},\\mathbf{\\zeta}_{%.2f}^{%.2f} $',time(ct_f),time(ct_i),time(ct_f),time(ct_i)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
+title(sprintf('$ \\Lambda_{%.2f}^{%.2f},\\mathbf{\\zeta}_{%.2f}^{%.2f} $',timeArr(ct_f),timeArr(ct_i),timeArr(ct_f),timeArr(ct_i)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
 colorbar('southoutside')
 
 % Compute FW-FTLE
 x0 = squeeze(xt_Advect_for(1,:)); y0 = squeeze(yt_Advect_for(1,:)); z0 = squeeze(zt_Advect_for(1,:));
 xf = squeeze(xt_Advect_for(end,:)); yf = squeeze(yt_Advect_for(end,:)); zf = squeeze(zt_Advect_for(end,:));
-tf = time(ct_f); ti = time(ct_i); % Lagrangian time interval of analysis 
+tf = timeArr(ct_f); ti = timeArr(ct_i); % Lagrangian time interval of analysis 
 
 Tri_pts_Uni = TrianT{1}; xfData = squeeze(x{end}); yfData = squeeze(y{end}); zfData = squeeze(z{end}); Trif_Data = squeeze(TrianT{end});
 
@@ -229,13 +229,13 @@ subplot(1,4,3)
 trisurf(Tri_pts_Uni,x0,y0,z0,lambdaIsoField,'FaceAlpha',1,'Edgecolor','none');
 colorbar('southoutside'); axis equal; shading interp;  axis off;
 view(viewAngle); camva(7);
-title(sprintf('$ {}_{iso}\\Lambda_{%.2f}^{%.2f} $',time(ct_i),time(ct_f)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
+title(sprintf('$ {}_{iso}\\Lambda_{%.2f}^{%.2f} $',timeArr(ct_i),timeArr(ct_f)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
 
 subplot(1,4,4)
 trisurf(Tri_pts_Uni,x0,y0,z0,lambdaField,'FaceAlpha',1,'Edgecolor','none');hold on 
 quiver3(x0,y0,z0,maxDefEigVec(1,:),maxDefEigVec(2,:),maxDefEigVec(3,:),'k','Linewidth',1,'ShowArrowHead','off'); hold off
 colorbar('southoutside'); axis equal; shading interp;  axis off;
 view(viewAngle); camva(7);
-title(sprintf('$ \\Lambda_{%.2f}^{%.2f},\\mathbf{\\zeta}_{%.2f}^{%.2f} $',time(ct_i),time(ct_f),time(ct_i),time(ct_f)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
+title(sprintf('$ \\Lambda_{%.2f}^{%.2f},\\mathbf{\\zeta}_{%.2f}^{%.2f} $',timeArr(ct_i),timeArr(ct_f),timeArr(ct_i),timeArr(ct_f)),'Interpreter','latex'); set(gca,'FontSize',fntSz);
 
 
